cad5819f1fcdb672e67bbb5fd3b5bd2eedda36ae
Michal Vanko <mklakakilli@gmail.com>
2015-04-20 16:29:28 +0200

    Added home stylings

--- src/home/home.js
+++ src/home/home.js
@@ -1,6 +1,7 @@
 import {inject} from 'aurelia-framework';
 import {HttpClient} from 'aurelia-http-client';
 import {PostService} from '../post-service';
+import './home.css!';
 
 @inject(HttpClient, PostService)
 export class Home {
@@ -14,6 +15,34 @@ export class Home {
   getLatestPosts() {
     // TODO this.http.get(this.postService.url)
     return [
+      {
+        "author" : {
+            "name" : "Tester",
+            "email" : "testing@awesome.com"
+        },
+        "body" : "<p>Krasny den</p> <p>Krasny den</p>  <p>Krasny den</p>  <p>Krasny den</p>  <p>Krasny den</p>  <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>Krasny den</p> <p>skareda noc</p>",
+        "comments" : [
+            {
+                "body" : "Hey, Tests are awesome",
+                "isReply" : false,
+                "date" : "2015-04-06T12:59:11.052Z",
+                "author" : {
+                    "name" : "Tester",
+                    "email" : "testing@awesome.com"
+                }
+            },
+            {
+                "body" : "Hey, Tests are still awesome",
+                "isReply" : false,
+                "date" : "2015-04-06T13:10:35.727Z",
+            }
+        ],
+        "createdDate" : "2015-04-05T22:24:32.633Z",
+        "lastUpdated" : "2015-04-05T22:24:32.632Z",
+        "seoTitle" : "esteraz",
+        "state" : "Published",
+        "title" : "skusmeto"
+      },
       {
         "author" : {
             "name" : "Tester",
